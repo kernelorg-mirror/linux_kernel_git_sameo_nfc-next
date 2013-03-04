8c8f4b42564d4977b438775f81dcc703e148b796
Thierry Escande <thierry.escande@linux.intel.com>
2013-03-04 15:43:32 +0100

    NFC: llcp: Add cleanup support for unreplied SNL requests
    If the remote LLC doesn't reply in time to our SNL requests we remove
    them from the list of pending requests. The timeout is fixed to an
    arbitrary value of 3 times remote_lto.
    
    When not replied, the local LLC broadcasts NFC_EVENT_LLC_SDRES nl events for
    the concerned uris with sap values set to LLCP_SDP_UNBOUND (which is 65).
    
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/llcp/llcp.h
+++ net/nfc/llcp/llcp.h
@@ -55,6 +55,8 @@ struct nfc_llcp_sdp_tlv {
 	u8 tid;
 	u8 sap;
 
+	unsigned long time;
+
 	struct hlist_node node;
 };
 
@@ -100,6 +102,8 @@ struct nfc_llcp_local {
 
 	struct mutex sdreq_lock;
 	struct hlist_head pending_sdreqs;
+	struct timer_list sdreq_timer;
+	struct work_struct sdreq_timeout_work;
 	u8 sdreq_next_tid;
 
 	/* sockets array */
