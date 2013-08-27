8ae7fdef755735fd327a6bf1e133654d184b69b5
Samuel Ortiz <sameo@linux.intel.com>
2013-08-28 00:39:48 +0200

    NFC: Define secure element IO API and commands
    In order to send and receive ISO7816 APDUs to and and NFC embedded
    secure elements, we define a specific netlink command.
    On a typical SE use case, host applications will send very few APDUs
    (Less than 10) per transaction. This is why we decided to go for a
    simple netlink API. Defining another NFC socket protocol for such low
    traffic would have been overengineered.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/net/nfc/nfc.h
+++ include/net/nfc/nfc.h
@@ -47,6 +47,9 @@ struct nfc_dev;
 typedef void (*data_exchange_cb_t)(void *context, struct sk_buff *skb,
 								int err);
 
+typedef void (*se_io_cb_t)(void *context, u8 *apdu, size_t apdu_len,
+								int err);
+
 struct nfc_target;
 
 struct nfc_ops {
@@ -73,6 +76,9 @@ struct nfc_ops {
 	int (*discover_se)(struct nfc_dev *dev);
 	int (*enable_se)(struct nfc_dev *dev, u32 se_idx);
 	int (*disable_se)(struct nfc_dev *dev, u32 se_idx);
+	int (*se_io) (struct nfc_dev *dev, u32 se_idx,
+		      u8 *apdu, size_t apdu_length,
+		      se_io_cb_t cb, void *cb_context);
 };
 
 #define NFC_TARGET_IDX_ANY -1
